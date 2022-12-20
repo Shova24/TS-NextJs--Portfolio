9a9e978fa5994969e0e70ff7b0f12f04792d06c2
Shova24 <jannatul35-2568@diu.edu.bd>
2022-12-20 13:31:20 +0600

    Hero section

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,12 +3,13 @@ import Image from 'next/image'
 import { Inter } from '@next/font/google'
 import styles from '../styles/Home.module.css'
 import Header from '../Components/Header'
+import Hero from '../Components/Hero'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <>
+    <div className="bg-[rgb(36,36,36)] text-white h-screen snap-mandatory overflow-scroll z-0">
     <Head>
       <title>Shova's Portfolio</title>
     </Head>
@@ -16,11 +17,14 @@ export default function Home() {
     <Header/>
 
     {/* Hero  */}
+    <section id='hero'className='snap-center'>
+      <Hero/>
+    </section>
     {/* About  */}
     {/* Experience */}
     {/* Skills */}
     {/* Projects  */}
     {/* Contact Me */}
-    </>
+    </div>
   )
 }
